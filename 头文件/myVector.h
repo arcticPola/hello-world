fba5c172e12b164df2019e307f86743a556a2654
arcticPola <381468957@qq.com>
2024-08-31 22:26:28 +0800

    lch 0831

--- 头文件/myVector.h
+++ 头文件/myVector.h
@@ -1,18 +1,38 @@
 #pragma once
+#include <initializer_list>
 
 template<typename T>
 class myVector
 {
 public:
+    using Iterator = T *;
+    using const_Iterator = const T *;
     myVector();
-    myVector(int s);
-    myVector(int s, T a);
+    myVector(int n);
+    myVector(int n, const T &val = T());
+    myVector(const myVector<T> &vec);
+    myVector(initializer_list<T> li);
     ~myVector();
+    template <typename iterator>
+    myVector(iterator firsh, iterator last);
+    
+    myVector<T> &operator=(const myVector<T> &vec);
+    T& operator[](int index) const ;
+    // const T& operator[] (int index) const;
+    const T& at(int index);
+    void push_back(T &a);
+    void push_back(T &&a);
+    T *begin();
+    T *end();
+    bool empty();
+
+    
 
 private:
-    T *m_elem;
-    int m_size;
-    int m_capacity;
+    // T *m_elem;
+    // int m_size;
+    // int m_capacity;
+    Iterator start;
+    Iterator finish;
+    Iterator end_of_storage;
 };
-
-
--- 源文件/myVector.cpp
+++ 源文件/myVector.cpp
@@ -1,27 +1,109 @@
 #include "myVector.h"
 #include <iostream>
+#include <initializer_list>
 
 template <typename T>
-inline myVector<T>::myVector():m_size(0),m_capacity(0)
+inline myVector<T>::myVector() : start(nullptr), finish(nullptr),end_of_storage(nullptr)
 {
 }
 
 template <typename T>
-myVector<T>::myVector(int s):m_size(s),m_capacity(s),m_elem(new T[s])
+myVector<T>::myVector(int n):start(new T[n * sizeof(T)]), finish(start + n),end_of_storage(finish)
 {
 }
 
 template <typename T>
-myVector<T>::myVector(int s, T a):m_size(s),m_capacity(s),m_elem(new T[s])
+myVector<T>::myVector(int n, const T&val = T()):start(new T[n * sizeof(T)]),finish(start + n),end_of_storage(finish)
 {
-    for (int i = 0;i<s;i++)
+    for (int i = 0; i < n; i++)
     {
-        m_elem[i] = a;
+        start[i] = val;
     }
+}
+
+template <typename T>
+myVector<T>::myVector(const myVector<T>& vec):m_size(vec.m_size),m_capacity(vec.m_capacity)
+{
+    delete m_elem;
+    this->m_elem = new T[m_size];
+    for (int i = 0; i < m_size;i++)
+    {
+        m_elem[i] = vec.m_elem[i];
+    }
+}
+
+template <typename T>
+myVector<T>::myVector(initializer_list<T> li):m_size(li.size()),m_capacity(li.size()),m_elem(new T[li.size()])
+{
+    auto it = li.begin();
+}
+
+template <typename T>
+myVector<T>::~myVector()
+{
+    m_size = 0;
+    m_capacity = 0;
+    delete m_elem;
+}
+
+template <typename T>
+const T& myVector<T>::at(int index)
+{
+    // TODO: insert return statement here
+}
+
+template <typename T>
+void myVector<T>::push_back(T &a)
+{
+}
+
+template <typename T>
+void myVector<T>::push_back(T &&a)
+{
+}
+
+template <typename T>
+T *myVector<T>::begin()
+{
+    return nullptr;
+}
+
+template <typename T>
+T *myVector<T>::end()
+{
+    return nullptr;
+}
+
+template <typename T>
+bool myVector<T>::empty()
+{
+    return false;
+}
+
+// template <typename T>
+// const T& myVector<T>::operator[](int index) const
+// {
+//     // TODO: insert return statement here
+// }
+
+template <typename T>
+myVector<T>& myVector<T>::operator=(const myVector<T>&vec)
+{
+    this->m_size = vec.m_size;
+    this->m_capacity = vec.
+
+}
+
+template <typename T>
+T& myVector<T>::operator[] (int index) const
+{
+    // TODO: 在此处插入 return 语句
 }
 
 int main()
 {
     std::cout << "kkfkk";
+    myVector<int> v1(6);
+    // cout << v1[3] << "\n";
     return 0;
 }
