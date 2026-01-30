1e2e6d2554d731c4928dccf9bd3994199aeb3442
arcticPola <381468957@qq.com>
2026-01-30 21:52:54 +0800

    0130

---
+++ myTestC/mydaily.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <functional>
+using namespace std;
+
+
+
+/// 冒泡排序
+///--------------------||
+///---------------|||||||
+void BubbleSort01(vector<int> &nums)
+{
+    int n = nums.size();
+    for (int i = 0; i < n - 1; i++)
+    {
+        bool isSwap = false; // 是否交换过
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (nums[j] > nums[j + 1])
+            {
+                swap(nums[j], nums[j + 1]);
+                isSwap = true;
+            }
+        }
+        /// 若一轮都没有交换过
+        if (isSwap == false)
+        {
+            return;
+        }
+    }
+}
+
+int main()
+{
+    vector<int> vec = {33, 2, 55, 32, 34, 22, 11, 45, 242, 52};
+    BubbleSort01(vec);
+    for(auto&&x : vec)
+    {
+        cout << x << endl;
+    }
+    return 0;
+}
