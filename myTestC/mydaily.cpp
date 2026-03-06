78a56a6f26862007a76bb11205dc1275ffde32ca
arcticPola <381468957@qq.com>
2026-03-07 00:31:36 +0800

    030711

--- myTestC/mydaily.cpp
+++ myTestC/mydaily.cpp
@@ -5,7 +5,6 @@
 using namespace std;
 
 
-
 /// 冒泡排序
 ///--------------------||
 ///---------------|||||||
@@ -30,6 +29,7 @@ void BubbleSort01(vector<int> &nums)
         }
     }
 }
+
 ///-------------------||
 ///|||----------------
 void BubbleSort02(vector<int>& nums)
@@ -53,10 +53,55 @@ void BubbleSort02(vector<int>& nums)
     }
 }
 
+///||---------------------
+///|||||------------------
+void BubbleSort03(vector<int>& nums)
+{
+    int n = nums.size();
+    for (int i = n - 1; i > 0;i--)
+    {
+        bool isSwap = false; // 是否交换过
+        for (int j = n - 1; j > n - 1 - i; j--)
+        {
+            if(nums[j] < nums[j-1])
+            {
+                swap(nums[j], nums[j - 1]);
+                isSwap = true;
+            }
+        }
+        if(isSwap == false)
+        {
+            return;
+        }
+    }
+}
+
+///||-------------------------
+///---------------------||||||
+void BubbleSort04(vector<int>& nums)
+{
+    int n = nums.size();
+    for (int i = n - 1; i > 0;i--)
+    {
+        bool isSwap = false; // 是否交换过
+        for (int j = 0; j < i;j++)
+        {
+            if(nums[j] > nums[j+1])
+            {
+                swap(nums[j], nums[j + 1]);
+                isSwap = true;
+            }
+        }
+        if(isSwap == false)
+        {
+            return;
+        }
+    }
+}
 int main()
 {
-    vector<int> vec = {33, 2, 55, 32, 34, 22, 11, 45, 242, 52};
-    BubbleSort02(vec);
+    vector<int> vec = {33, 2, 55, 32, 34, 22, 59,11,55, 45, 242, 52};
+    BubbleSort04(vec);
     for(auto&&x : vec)
     {
         cout << x << endl;
